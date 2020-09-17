2994582879f580c95cc9eb2e1283a60e54abf681
wangsimin <taoyiran815579058@outlook.com>
2020-09-17 12:58:42 +0800

    update

--- src/main/java/LC0_200/LC50_100/LC93_Restore_IP_Addresses.java
+++ src/main/java/LC0_200/LC50_100/LC93_Restore_IP_Addresses.java
@@ -1,16 +1,25 @@
 package LC0_200.LC50_100;
 
+import org.junit.Test;
+
+
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ *  Runtime: 1ms   faster than 99.64%
+ *  Memory Usage: 38.3 MB   less than 96.64%
+ *  Sep 17 12:53 PM in Hangzhou
+ */
 
 public class LC93_Restore_IP_Addresses {
 
     private List<String> result = new ArrayList<String>();
+    private StringBuilder sb = new StringBuilder();
 
     public List<String> restoreIpAddresses(String s) {
-
-
-        return null;
+        backTrace(s, 1);
+        return result;
     }
 
     /**
@@ -19,12 +28,61 @@ public class LC93_Restore_IP_Addresses {
      *  @param s   the IP string
      *  @param index   there are 4 parts of IP segments, index represents which part is currently used
      */
-    public void backTrace(String s, int index, int hasUsed) {
+    public void backTrace(String s, int index) {
 
-        // the max left length is larger, so it cannot be restored to a IP address
-        if ((4 - index) * 3 + 3 - hasUsed < s.length())
+        if (index == 4 && s.length() <= 3 && s.length() >= 1 && checkIfVaild(s)) {
+            sb.append(s);
+            result.add(sb.toString());
+            sb.delete(sb.length() - s.length(), sb.length());
+            return;
+        } else if (index == 4)
             return;
 
+        // 取一位
+        if (s.length() >= 1 && checkIfVaild(s.substring(0, 1)) && s.length() - 1 <= (4 - index) * 3 && s.length() - 1 >= (4 - index)) {
+            sb.append(s.charAt(0));
+            sb.append('.');
+            backTrace(s.substring(1), index + 1);
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        // 取二位
+        if (s.length() >= 2 && checkIfVaild(s.substring(0, 2)) && s.length() - 2 <= (4 - index) * 3 && s.length() - 1 >= (4 - index)) {
+            sb.append(s.substring(0, 2));
+            sb.append('.');
+            backTrace(s.substring(2), index + 1);
+            sb.delete(sb.length() - 3, sb.length());
+        }
+
+        // 取三位
+        if (s.length() >= 3 && checkIfVaild(s.substring(0, 3)) && s.length() - 3 <= (4 - index) * 3 && s.length() - 1 >= (4 - index)) {
+            sb.append(s.substring(0, 3));
+            sb.append('.');
+            backTrace(s.substring(3), index + 1);
+            sb.delete(sb.length() - 4, sb.length());
+        }
+    }
+
+    private boolean checkIfVaild(String s) {
+        if (s.startsWith("0") && s.length() > 1)
+            return false;
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                result = result * 10 + s.charAt(i) - '0';
+            } else
+                return false;
+        }
+        if (result >=0 && result <= 255)
+            return true;
+        return false;
+    }
+
+    @Test
+    public void test() {
+        restoreIpAddresses("0000");
     }
 
 }
