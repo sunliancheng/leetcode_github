79a986ff6d6071d9e1e1f1bebedbd5780c8b6d50
wangsimin <taoyiran815579058@outlook.com>
2020-10-17 19:56:00 +0800

    update

--- src/main/java/LC0_200/LC0_50/LC33_Search_In_Rotated_Sorted_Array.java
+++ src/main/java/LC0_200/LC0_50/LC33_Search_In_Rotated_Sorted_Array.java
@@ -4,11 +4,11 @@ public class LC33_Search_In_Rotated_Sorted_Array {
 
     public int search(int[] nums, int target) {
 
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0; i < nums.length; ++i)
             if (nums[i] == target)
                 return i;
-        }
         return -1;
+
     }
 
 }
