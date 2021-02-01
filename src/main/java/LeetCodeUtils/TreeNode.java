fa2b2546887b8d3570ea206a4328505a07df1da0
龙哥 <d>
2021-02-01 16:56:29 +0800

    s

--- src/main/java/LeetCodeUtils/TreeNode.java
+++ src/main/java/LeetCodeUtils/TreeNode.java
@@ -2,6 +2,9 @@ package LeetCodeUtils;
 
 import org.junit.Test;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class TreeNode {
 
     public int val;
@@ -23,12 +26,50 @@ public class TreeNode {
      */
     public TreeNode createTreeByLevelTraverse(String s) {
         String[] strs = s.split(",");
-        return null;
+        Deque<TreeNode> deque = new LinkedList<>();
+        Deque<TreeNode> currentLevel = new LinkedList<>();
+        int i = 0;
+        TreeNode root = new TreeNode(i);
+        deque.addLast(root);
+        while (++i < strs.length) {
+            TreeNode father = deque.getFirst();
+            TreeNode left = new TreeNode(i);
+            father.left = left;
+            if (++i < strs.length) {
+                TreeNode right = new TreeNode(++i);
+                father.right = right;
+            }
+        }
+        return root;
     }
 
-    @Test
-    public void test() {
 
+    public static void main(String[] args) {
+        TreeNode tree = createTree(3);
+        System.out.println(tree.val);
+        System.out.println(tree.left.val);
+        System.out.println(tree.right.val);
+        System.out.println(tree.left.left.val);
+    }
+
+
+
+
+    public static TreeNode createTree(int len) {
+        Deque<TreeNode> deque = new LinkedList<>();
+        int i = 0;
+        TreeNode root = new TreeNode(i);
+        deque.addLast(root);
+        while (++i < len) {
+            TreeNode father = deque.getFirst();
+            TreeNode left = new TreeNode(i);
+            father.left = left;
+            if (++i < len) {
+                TreeNode right = new TreeNode(i);
+                father.right = right;
+            }
+        }
+        return root;
     }
 
 
