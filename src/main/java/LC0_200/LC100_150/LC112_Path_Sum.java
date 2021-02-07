0899c519958107e3c0c8b7937e2b079e1b1521a5
wangsimin <taoyiran815579058@outlook.com>
2021-02-07 10:14:36 -0500

    54

--- src/main/java/LC0_200/LC100_150/LC112_Path_Sum.java
+++ src/main/java/LC0_200/LC100_150/LC112_Path_Sum.java
@@ -7,12 +7,8 @@ public class LC112_Path_Sum {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
         if (root.val == sum && root.right == null && root.left == null) return true;
-        if (root.left != null) {
-            root.left.val += root.val;
-        }
-        if (root.right != null) {
-            root.right.val += root.val;
-        }
+        if (root.left != null) root.left.val += root.val;
+        if (root.right != null) root.right.val += root.val;
         return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 
