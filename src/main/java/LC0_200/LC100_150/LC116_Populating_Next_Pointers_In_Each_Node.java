98d24a829f6a83dae87adb6178b123dbd7f1b684
龙哥 <d>
2021-02-08 11:41:05 +0800

    s

---
+++ src/main/java/LC0_200/LC100_150/LC116_Populating_Next_Pointers_In_Each_Node.java
@@ -0,0 +1,9 @@
+package LC0_200.LC100_150;
+
+public class LC116_Populating_Next_Pointers_In_Each_Node {
+
+    public Node connect(Node root) {
+
+    }
+
+}
--- src/main/java/LeetCodeUtils/Node.java
+++ src/main/java/LeetCodeUtils/Node.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 public class Node {
     public int val;
+    public Node left;
+    public Node right;
+    public Node next;
     public List<Node> children;
-
     public Node() {}
-
     public Node(int _val) {
         val = _val;
     }
@@ -15,5 +16,12 @@ public class Node {
     public Node(int _val, List<Node> _children) {
         val = _val;
         children = _children;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
     }
 }
