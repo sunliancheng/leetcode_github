f721821f9b69ebfd0d9fb7fd61d73e33de9d1efe
龙哥 <d>
2021-02-25 15:07:06 +0800

    s

--- src/main/java/LC0_200/LC100_150/LC132_Palindrome_Partitioning2.java
+++ src/main/java/LC0_200/LC100_150/LC132_Palindrome_Partitioning2.java
@@ -1,9 +1,32 @@
 package LC0_200.LC100_150;
 
+import org.junit.Test;
+
 public class LC132_Palindrome_Partitioning2 {
 
-    public int minCut(String s) {
-
+    @Test
+    public void test() {
+        int r = minCut("apjesgpsxoeiokmqmfgvjslcjukbqxpsobyhjpbgdfruqdkeiszrlmtwgfxyfostpqczidfljwfbbrflkgdvtytbgqalguewnhvvmcgxboycffopmtmhtfizxkmeftcucxpobxmelmjtuzigsxnncxpaibgpuijwhankxbplpyejxmrrjgeoevqozwdtgospohznkoyzocjlracchjqnggbfeebmuvbicbvmpuleywrpzwsihivnrwtxcukwplgtobhgxukwrdlszfaiqxwjvrgxnsveedxseeyeykarqnjrtlaliyudpacctzizcftjlunlgnfwcqqxcqikocqffsjyurzwysfjmswvhbrmshjuzsgpwyubtfbnwajuvrfhlccvfwhxfqthkcwhatktymgxostjlztwdxritygbrbibdgkezvzajizxasjnrcjwzdfvdnwwqeyumkamhzoqhnqjfzwzbixclcxqrtniznemxeahfozp");
+        System.out.println(r);
     }
+
+    public int minCut(String s) {
+        int n = s.length();
+        int cuts[] = new int[n];
+        boolean isPalindrome[][] = new boolean[n][n];
+        for(int i = 0; i < n; i++){
+            int minCuts = Integer.MAX_VALUE;
+            for(int j = i;j >= 0; j--){
+                if(s.charAt(i) == s.charAt(j) && (j + 1 >= i - 1 || isPalindrome[j + 1][i - 1])){
+                    isPalindrome[j][i] = true;
+                    minCuts = Math.min(minCuts, j - 1 < 0 ? 0 : cuts[j - 1] + 1);
+                }
+            }
+            cuts[i] = minCuts;
+        }
+        return cuts[n-1];
+    }
+
+
 
 }
