fd1c1e0b69db9fb4de2fe7dc8e4008b59aeccd4c
wangsimin <taoyiran815579058@outlook.com>
2021-03-03 19:57:28 +0800

    54

--- src/main/java/LC0_200/LC150_200/LC174_Dungeon_Game.java
+++ src/main/java/LC0_200/LC150_200/LC174_Dungeon_Game.java
@@ -9,34 +9,27 @@ public class LC174_Dungeon_Game {
     @Test
     public void test() {
         int[][] d = MyMatrix.IntMatrixAdapter("[[0,-74,-47,-20,-23,-39,-48],[37,-30,37,-65,-82,28,-27],[-76,-33,7,42,3,49,-93],[37,-41,35,-16,-96,-56,38],[-52,19,-37,14,-65,-42,9],[5,-26,-30,-65,11,5,16],[-60,9,36,-36,41,-47,-86],[-22,19,-5,-41,-8,-96,-95]]", 8, 7);
-        System.out.println(calculateMinimumHP(d));
+        int[][] v = MyMatrix.IntMatrixAdapter("[[-2,-3,3],[-5,-10,1],[10,30,-5]]", 3,3);
+        System.out.println(calculateMinimumHP(v));
     }
-
-    int result = Integer.MAX_VALUE, initialBlood = 0, left = 0;
 
     public int calculateMinimumHP(int[][] dungeon) {
-        MyPrint.print2DMatrix(dungeon);
-        dfs(dungeon, 0, 0, new boolean[dungeon.length][dungeon[0].length]);
-        return 1 + result;
+        int[][] memory = new int[dungeon.length][dungeon[0].length];
+        int ans = dfs(dungeon, 0, 0, memory);
+        return 1 + ans;
     }
 
-    public void dfs(int[][] board, int i, int j, boolean[][] visited) {
-        if (i <0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]) return;
-        int ori_init = initialBlood, ori_left = left;
-        left = left + board[i][j]; initialBlood = initialBlood < left ? initialBlood : left;
-        visited[i][j] = true;
+    public int dfs(int[][] board, int i, int j, int[][] memory) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) return Integer.MAX_VALUE;
+        if (memory[i][j] != 0) return memory[i][j];
 
         if (i == board.length - 1 && j == board[0].length - 1) {
-            MyPrint.print2DMatrix(visited);
-            result = result > -initialBlood ? -initialBlood : result;
-            initialBlood = ori_init; left = ori_left; visited[i][j] = false;
-            return;
+            memory[i][j] = -board[i][j] > 0 ? -board[i][j] : 0;
+            return memory[i][j];
         }
 
-        dfs(board, i + 1, j, visited);
-        dfs(board, i - 1, j, visited);
-        dfs(board, i, j + 1, visited);
-        dfs(board, i, j - 1, visited);
-        initialBlood = ori_init; left = ori_left; visited[i][j] = false;
+        int t = Math.min(dfs(board, i + 1, j, memory), dfs(board, i, j + 1, memory)) - board[i][j];
+        memory[i][j] = t > 0 ? t : 0;
+        return memory[i][j];
     }
 }
---
+++ src/main/java/LC200_400/LC250_300/LC278_First_Bad_Version.java
@@ -0,0 +1,25 @@
+package LC200_400.LC250_300;
+
+public class LC278_First_Bad_Version {
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean isBadVersion(int i) {
+        return true;
+    }
+
+}
+
+
