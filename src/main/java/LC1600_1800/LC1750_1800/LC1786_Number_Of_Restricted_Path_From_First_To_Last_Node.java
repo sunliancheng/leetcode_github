a3572f75b7beb37a4871b13b8f0bafc939c85187
龙哥 <d>
2021-03-08 17:09:10 +0800

    s

--- src/main/java/LC1600_1800/LC1750_1800/LC1786_Number_Of_Restricted_Path_From_First_To_Last_Node.java
+++ src/main/java/LC1600_1800/LC1750_1800/LC1786_Number_Of_Restricted_Path_From_First_To_Last_Node.java
@@ -1,4 +1,9 @@
 package LC1600_1800.LC1750_1800;
 
 public class LC1786_Number_Of_Restricted_Path_From_First_To_Last_Node {
+
+    public int countRestrictedPaths(int n, int[][] edges) {
+
+    }
+
 }
