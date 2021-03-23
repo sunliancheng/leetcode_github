88930dfd94a466c831a590e5115bb2f47b5649af
龙哥 <d>
2021-03-23 14:41:06 +0800

    s

--- src/main/java/LC200_400/LC300_350/LC310_Minimum_Height_Trees.java
+++ src/main/java/LC200_400/LC300_350/LC310_Minimum_Height_Trees.java
@@ -10,69 +10,45 @@ public class LC310_Minimum_Height_Trees {
 
     @Test
     public void test() {
-        findMinHeightTrees(6, MyMatrix.IntMatrixAdapter("[[3,0],[3,1],[3,2],[3,4],[5,4]]", 5, 2));
-        findMinHeightTrees(2, new int[][]{{0, 1}});
+        System.out.println(findMinHeightTrees(6, MyMatrix.IntMatrixAdapter("[[3,0],[3,1],[3,2],[3,4],[5,4]]", 5, 2)));;
+        System.out.println(findMinHeightTrees(2, new int[][]{{0, 1}}));
 
     }
 
-    int min = Integer.MAX_VALUE;
-    List<Integer> res = new ArrayList<>();
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        int[][] matrix = new int[n][n];
+        List<Integer> res = new ArrayList<>();
+        if (n == 1) {
+            res.add(0);
+            return res;
+        }
+        HashSet<Integer>[] graph = new HashSet[n];
+        for (int i = 0; i < n; ++i) graph[i] = new HashSet<>();
         int[] edge_size = new int[n];
         for (int[] edge : edges) {
             ++edge_size[edge[0]];
             ++edge_size[edge[1]];
-            matrix[edge[0]][edge[1]] = 1;
-            matrix[edge[1]][edge[0]] = 1;
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
         }
 
-        //MyPrint.print2DMatrix(matrix);
-        boolean flag = false;
-        // do bfs search for each node that has more than one edges
-        for (int i = 0; i < n; ++i) {
-            if (edge_size[i] == 1 || edge_size[i] == 0)
-                continue;
-            flag = true;
-            bfs(i, matrix);
-        }
-        //MyPrint.print2DMatrix(matrix);
-        if (!flag) {
-            for (int i = 0; i < n; ++i)
-                bfs(i, matrix);
-        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < edge_size.length; ++i)
+            if (edge_size[i] == 1) q.offer(i);
 
+        while (!q.isEmpty()) {
+            res = new ArrayList<>();
+            int size = q.size();
+            while (--size >= 0) {
+                int cur = q.poll();
+                res.add(cur);
+                for (int v : graph[cur]) {
+                    if (--edge_size[v] == 1) q.offer(v);
+                }
+            }
+        }
         return res;
     }
 
-    void bfs(int i, int[][] matrix) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(i);
-        Set<Integer> set = new HashSet<>();
-        set.add(i);
-        int level = 0;
-        while (!queue.isEmpty()) {
-            int n = queue.size();
-            ++level;
-            if (level > min)
-                return;
-            while (--n >= 0) {
-                int root = queue.poll();
-                for (int j = 0; j < matrix.length; ++j) {
-                    if (matrix[root][j] == 1 && !set.contains(j)) {
-                        queue.offer(j);
-                        //System.out.print(j + " 第 " + level + "层");
-                        set.add(j);
-                    }
-                }
-                //System.out.println();
-            }
-        }
-        if (level < min) {
-            min = level;
-            res.clear();
-        }
-            res.add(i);
-    }
+
 
 }
