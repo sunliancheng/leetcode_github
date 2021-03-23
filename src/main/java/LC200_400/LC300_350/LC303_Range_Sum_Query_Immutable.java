e36f15e8584aee2fca7b47822fc18aed625bec2f
wangsimin <taoyiran815579058@outlook.com>
2021-03-23 23:28:40 +0800

    54

--- src/main/java/LC200_400/LC300_350/LC303_Range_Sum_Query_Immutable.java
+++ src/main/java/LC200_400/LC300_350/LC303_Range_Sum_Query_Immutable.java
@@ -6,16 +6,16 @@ public class LC303_Range_Sum_Query_Immutable {
 
     @Test
     public void test() {
-        NumArray numArray = new NumArray(new int[]{-2,0,3,-5,2,-1});
-        System.out.println(numArray.sumRange(0,2));
+        NumArray2 numArray2 = new NumArray2(new int[]{-2,0,3,-5,2,-1});
+        System.out.println(numArray2.sumRange(0,2));
     }
 
 }
 
-class NumArray {
+class NumArray2 {
 
     int[] psum;
-    public NumArray(int[] nums) {
+    public NumArray2(int[] nums) {
         psum = new int[nums.length + 1];
         for (int i = 0; i < nums.length; ++i) {
             psum[i + 1] = nums[i];
