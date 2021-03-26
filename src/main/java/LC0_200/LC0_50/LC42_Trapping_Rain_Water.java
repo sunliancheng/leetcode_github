55531e74e04ef878d7f3f340a73d92d0aec58c13
龙哥 <d>
2021-03-26 11:06:40 +0800

    s

--- src/main/java/LC0_200/LC0_50/LC42_Trapping_Rain_Water.java
+++ src/main/java/LC0_200/LC0_50/LC42_Trapping_Rain_Water.java
@@ -2,90 +2,49 @@ package LC0_200.LC0_50;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class LC42_Trapping_Rain_Water {
 
     @Test
     public void test() {
-        System.out.println(trap_DP(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));;
-    }
-
-    /**
-     * 3. using stacks
-     */
-    public int trap_Stacks(int [] height) {
-        int ans = 0;
-
-
-        return ans;
-    }
-
-
-    /**
-     * 1. brute force
-     * @param height
-     * @return
-     */
-    public int trap_BF(int[] height) {
-        int ans = 0;
-        for (int i = 0; i < height.length; ++i) {
-            int left_max = 0, right_max = 0;
-            int l = i, r = i;
-            while (l >= 1) {
-                --l;
-                left_max = left_max > height[l] ? left_max : height[l];
-            }
-            while (r < height.length - 1) {
-                ++r;
-                right_max = right_max > height[r] ? right_max : height[r];
-            }
-            int tem = Math.min(right_max, left_max) - height[i];
-            ans += tem <= 0 ? 0 : tem;
-        }
-        return ans;
-    }
-
-
-    /**
-     * 2. dynamic programming
-     * @param height
-     * @return
-     */
-    public int trap_DP(int[] height) {
-        int ans = 0;
-        int[] dp1 = new int[height.length];
-        int[] dp2 = new int[height.length];
-
-        int left = 0, right = 0;
-        for (int i = 0; i < height.length; ++i) {
-            left = left > height[i] ? left : height[i];
-            dp1[i] = left;
-            right = right > height[height.length - 1 - i] ? right : height[height.length - 1 - i];
-            dp2[height.length - 1 - i] = right;
-        }
-        for (int i = 0; i < height.length; ++i) {
-            ans += Math.min(dp1[i], dp2[i]) - height[i];
-        }
-        return ans;
-
+        System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
     }
 
     public int trap(int[] height) {
-
-        if (height == null)
-            return 0;
-        int ans = 0;
-        int size = height.length;
-
-        int[] left_max = new int[size], right_max = new int[size];
-
-        left_max[0] = height[0];
-        for (int i = 0; i < size; ++i) {
-            left_max[i] = Math.max(height[i], left_max[i - 1]);
+        if (height.length == 0 || height.length == 1) return 0;
+        int n = height.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.offer(new int[]{0, height[0]});
+        pq.offer(new int[]{n - 1, height[n - 1]});
+        int result = 0;
+        boolean[] visited = new boolean[height.length];
+        while (pq.size() > 0) {
+            int[] tem = pq.poll();
+            result += dfs(pq, height, tem[0], tem[1], visited);
         }
-        for (int i = 1; i < size; ++i) {
-            ans += Math.min(left_max[i], right_max[i]) - height[i];
-        }
-        return ans;
+        return result;
     }
+
+    public int dfs(PriorityQueue<int[]> pq, int[] height, int i, int h, boolean[] visited) {
+        int water = 0;
+        if (h >= height[i]) {
+            water += h - height[i];
+            visited[i] = true;
+            List<Integer> list = new ArrayList<>();
+            list.add(i + 1); list.add(i - 1);
+            for (int x : list)
+                if (x >= 0 && x < height.length && !visited[x]) water += dfs(pq, height, x, h, visited);
+
+        } else {
+            visited[i] = true;
+            pq.add(new int[]{i, height[i]});
+        }
+        return water;
+    }
+
+
 
 }
--- src/main/java/LC400_600/LC400_450/LC407_Trapping_Rain_Water2.java
+++ src/main/java/LC400_600/LC400_450/LC407_Trapping_Rain_Water2.java
@@ -1,7 +1,6 @@
 package LC400_600.LC400_450;
 
 import LeetCodeUtils.MyMatrix;
-import LeetCodeUtils.MyPrint;
 import org.junit.Test;
 
 import java.util.PriorityQueue;
@@ -19,53 +18,44 @@ public class LC407_Trapping_Rain_Water2 {
         int m = heightMap.length, n = heightMap[0].length;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
 
-        int[][] visited = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
         for (int i = 0; i < m; ++i) {
-            visited[i][0] = -1;
+            visited[i][0] = true;
             pq.add(new int[]{i, 0, heightMap[i][0]});
-            visited[i][n - 1] = -1;
+            visited[i][n - 1] = true;
             pq.add(new int[]{i, n - 1, heightMap[i][n - 1]});
         }
 
         for (int j = 1; j < n - 1; ++j) {
-            visited[0][j] = -1;
+            visited[0][j] = true;
             pq.add(new int[]{0, j, heightMap[0][j]});
-            visited[m - 1][j] = -1;
+            visited[m - 1][j] = true;
             pq.add(new int[]{m - 1, j, heightMap[m - 1][j]});
         }
 
         int result = 0;
         while (pq.size() > 0) {
             int[] node = pq.poll();
-            handle(pq, heightMap, node[0], node[1], node[2], visited);
+            result += dfs(pq, heightMap, node[0], node[1], node[2], visited);
         }
-        //MyPrint.print2DMatrix(heightMap);
-        //MyPrint.print2DMatrix(visited);
-        pq.clear();
-        for (int i = 0; i < visited.length; ++i)
-            for (int j = 0; j < visited[0].length; ++j)
-                if (visited[i][j] != -1) result += visited[i][j] - heightMap[i][j];
-
         return result;
     }
 
-    public void handle (PriorityQueue<int[]> pq, int[][] heightMap, int i, int j, int h, int[][] visited) {
-        if (h <= heightMap[i][j]) {
-            visited[i][j] = -1;
+    public int dfs (PriorityQueue<int[]> pq, int[][] heightMap, int i, int j, int h, boolean[][] visited) {
+        int water = 0;
+        if (h >= heightMap[i][j]) {
+            visited[i][j] = true;
+            water += h - heightMap[i][j];
             for (int k = 0; k < directions.length - 1; ++k) {
                 int x = directions[k] + i, y = directions[k + 1] + j;
-                if (x >= 0 && y >= 0 && x <= visited.length - 1 && y <= visited[0].length - 1 && visited[x][y] != -1)
-                    handle(pq, heightMap, x, y, heightMap[i][j], visited);
+                if (x >= 0 && y >= 0 && x < heightMap.length && y < heightMap[0].length && !visited[x][y])
+                    water += dfs(pq, heightMap, x, y, h, visited);
             }
         } else {
-            if (visited[i][j] == 0) visited[i][j] = h;
-            visited[i][j] = Math.min(visited[i][j], h);
-            for (int k = 0; k < directions.length - 1; ++k) {
-                int x = directions[k] + i, y = directions[k + 1] + j;
-                if (x >= 0 && y >= 0 && x <= visited.length - 1 && y <= visited[0].length - 1 && visited[x][y] != -1 && visited[x][y] != 0)
-                    visited[i][j] = Math.min(visited[i][j], visited[x][y]);
-            }
+            visited[i][j] = true;
+            pq.add(new int[]{i, j, heightMap[i][j]});
         }
+        return water;
     }
 
 }
