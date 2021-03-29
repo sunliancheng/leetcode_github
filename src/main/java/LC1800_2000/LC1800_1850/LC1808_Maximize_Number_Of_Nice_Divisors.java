e8f42b28e97660bdd08e68694679b293abe3887b
wangsimin <taoyiran815579058@outlook.com>
2021-03-29 18:27:50 +0800

    54

--- src/main/java/LC1800_2000/LC1800_1850/LC1808_Maximize_Number_Of_Nice_Divisors.java
+++ src/main/java/LC1800_2000/LC1800_1850/LC1808_Maximize_Number_Of_Nice_Divisors.java
@@ -3,7 +3,21 @@ package LC1800_2000.LC1800_1850;
 public class LC1808_Maximize_Number_Of_Nice_Divisors {
 
     public int maxNiceDivisors(int primeFactors) {
+
         return 1;
+    }
+
+    public long quickPow(int p, int q, int MOD) {
+        long ans = 1L;
+        long base = p;
+        while (q != 0) {
+            if ((q & 1) == 1) {
+                ans = ans * base % MOD;
+            }
+            base = base * base % MOD;
+            q >>>= 1;
+        }
+        return ans;
     }
 
 }
