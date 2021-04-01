2356566ab6a46c809f0c2c31f61774ed6bfce2bc
wangsimin <taoyiran815579058@outlook.com>
2021-04-01 10:42:58 +0800

    54

--- src/main/java/LC400_600/LC500_550/LC514_Freedom_Trail.java
+++ src/main/java/LC400_600/LC500_550/LC514_Freedom_Trail.java
@@ -8,15 +8,17 @@ public class LC514_Freedom_Trail {
 
     @Test
     public void test() {
-        System.out.println(findRotateSteps("dcba", "abcd"));
+
+        //"xrrakuulnczywjs"
+        //"jrlucwzakzussrlckyjjsuwkuarnaluxnyzcnrxxwruyr"
+        String ring = "godding";
+        String key = "gd";
+        System.out.println(findRotateSteps(ring, key));
     }
 
-    int result = Integer.MAX_VALUE;
-
     public int findRotateSteps(String ring, String key) {
-        int n = ring.length();
         Map<Character, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < ring.length(); ++i) {
             char c = ring.charAt(i);
             if (map.containsKey(c)) {
                 map.get(c).add(i);
@@ -26,31 +28,35 @@ public class LC514_Freedom_Trail {
                 map.put(c, list);
             }
         }
-        int[][] memo = new int[n][26];
-
-        dfs(0, key, 0, map, memo, n, key.length());
-
-        return result;
+        int[][] memo = new int[ring.length()][key.length()];
+        return dfs(0, key, 0, map, memo, ring.length());
     }
 
-    public void dfs (int cur, String key, int level, Map<Character, List<Integer>> map, int[][] memo, int n, int tem) {
-        if (level == key.length()) {
-            result = result < tem ? result : tem;
-            return;
-        }
+    public int dfs (int cur, String key, int level, Map<Character, List<Integer>> map, int[][] memo, int n) {
+        if (level == key.length()) { return 0; }
         char c = key.charAt(level);
-        List<Integer> list = map.get(c);
+        if (memo[cur][level] != 0) return memo[cur][level];
+        List<Integer> list = map.get(c); int min = Integer.MAX_VALUE;
         for (int i : list) {
-            int anticlockwise = Math.abs(cur - i), clockwise = n - anticlockwise;
-            dfs(getCur(n, cur + anticlockwise), key, 1 + level, map, memo, n, anticlockwise + tem);
-            dfs(getCur(n, cur - clockwise), key, 1 + level, map, memo, n, clockwise + tem);
+            int anticlockwise = getAnticlockwise(cur, i, n), clockwise = getClockwise(cur, i, n);
+            int r1 = anticlockwise + dfs(i, key, 1 + level, map, memo, n);
+            int r2 = clockwise + dfs(i, key, 1 + level, map, memo, n);
+            min = Math.min(Math.min(r1, r2), min);
         }
+        memo[cur][level] = 1 + min;
+        return memo[cur][level];
     }
 
-    public int getCur(int n, int cur) {
-        cur = cur % (n - 1);
-        if (cur < 0) cur += n;
-        return cur;
+    public int getAnticlockwise(int cur, int target, int n) {
+        int offSet = target - cur;
+        if (offSet < 0) offSet = n + offSet;
+        return offSet;
+    }
+
+    public int getClockwise(int cur, int target, int n) {
+        int offSet = cur - target;
+        if (offSet < 0) offSet = n + offSet;
+        return offSet;
     }
 
 
