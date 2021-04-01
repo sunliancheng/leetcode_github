23d169665e958f52c315675816a7444d24935566
wangsimin <taoyiran815579058@outlook.com>
2021-04-01 14:36:14 +0800

    54

--- src/main/java/LC600_800/LC650_700/LC685_Redundant_Connection2.java
+++ src/main/java/LC600_800/LC650_700/LC685_Redundant_Connection2.java
@@ -3,7 +3,7 @@ package LC600_800.LC650_700;
 public class LC685_Redundant_Connection2 {
 
     public int[] findRedundantDirectedConnection(int[][] edges) {
-
+        int root = edges[0][1];
     }
 
 }
